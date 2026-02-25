f85a3d2118b231fe9f8556358f5110d0984e89e0
angeloren92 <angeloren92@gmail.com>
2026-02-25 12:47:55 +0100

    adj styling add responsive

--- js/main.js
+++ js/main.js
@@ -7,16 +7,16 @@ fetch(endpoint)
         //console.log(data)
         let markup = '';
         for (let i = 0; i < data.length; i++) {
-            const { id, title, date, url } = data[i];
+            const { title, date, url } = data[i];
             //console.log(id, title, date, url)
             markup += `                
-            <div class="col">
+            <div class="col-12 col-md-6 col-lg-4">
                 <div class="card rounded-0 position-relative">
                     <img src="./img/pin.svg" alt="" class="position-absolute start-50 translate-middle-x">
                     <figure class="card-body">
                         <img src="${url}" alt="${title}" class="img-fluid mb-3">
-                        <h2 class="h5 text-muted">${title}</h2>
-                        <p class="text-muted mb-2">${date}</p>
+                        <p class="text-muted mb-0">${date}</p>
+                        <h2 class="h4 fw-bold">${title.toUpperCase()}</h2>
                     </figure>
                 </div>
             </div>`
