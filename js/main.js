6a84b47ed72b22d25e5a0bb0b8406675e0a036db
angeloren92 <angeloren92@gmail.com>
2026-02-26 12:19:09 +0100

    add event listener markup

--- js/main.js
+++ js/main.js
@@ -6,37 +6,34 @@ fetch(endpoint)
     .then((data) => {
         const markup = getMarkup(data);
         rowEl.insertAdjacentHTML('beforeend', markup);
-        
+
         const cardImgEl = document.querySelectorAll('#cardImg');
-        
-        getOverlayMarkup(cardImgEl, data);
 
+
+        cardImgEl.forEach((element) => {
+            element.addEventListener('click', function () {
+                const src = element.src;
+                const alt = element.alt;
+                console.log(src, alt)
+                const overlayMarkup = `
+                    <div id="overlay">
+                        <div class="row-cols-1 my-4">
+                            <div class="col">
+                                <button class="mb-5" onclick="off()">Chiudi</button>
+                            </div>
+                            <div class="col">
+                                <img src="${src}" alt="${alt}" class="img-fluid w-50">
+                            </div>
+                        </div>
+                    </div>
+                `
+                console.log(overlayMarkup)
+            })
         })
 
+    })
 
 
-function getOverlayMarkup(nodes, data) {
-    let markup = '';
-    nodes.forEach((element) => { 
-        const src = element.src;
-        const alt = element.alt;
-        console.log(src, alt)
-        markup = `
-            <div id="overlay">
-                <div class="row-cols-1 my-4">
-                    <div class="col">
-                        <button class="mb-5" onclick="off()">Chiudi</button>
-                    </div>
-                    <div class="col">
-                        <img src="${src}" alt="${alt}" class="img-fluid w-50">
-                    </div>
-                </div>
-            </div>
-        `
-        })
-
-console.log(markup)
-}
 
 
 
